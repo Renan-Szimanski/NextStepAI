49d8271ae28d89384a5eb475b89c69783158313a
renan <renan.szi10@gmail.com>
2026-05-14 08:42:36 -0300

    Fix: Resolve erro do supaase no build

--- src/app/api/auth/recuperar-senha/route.ts
+++ src/app/api/auth/recuperar-senha/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 
 export async function POST(req: Request) {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  const supabase = createClient(supabaseUrl, supabaseAnonKey);
   try {
     const { email } = await req.json();
 
